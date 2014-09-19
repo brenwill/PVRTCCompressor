e4d55cc59881a276e8d05975810e4d8151995f04
Brendan Duncan <brendanduncan@gmail.com>
2014-09-19 13:38:56 -0700

    fix BITSCALE LUTS

--- gen_bitscales.cpp
+++ gen_bitscales.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
   std::cout << "#include \"BitScale.h\"" << std::endl << std::endl;
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_5_TO_8[32] = {" << std::endl;
   for (int i = 0; i < 32; ++i) {
-    std::cout << " " << ((i * 8));
+    std::cout << " " << ((i * 255) / 31);
     if (i != 31) {
       std::cout << ",";
     }
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_4_TO_8[16] = {" << std::endl;
   for (int i = 0; i < 16; ++i) {
-    std::cout << " " << ((i * 16));
+    std::cout << " " << ((i * 255) / 15);
     if (i != 15) {
       std::cout << ",";
     }
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_3_TO_8[8] = {" << std::endl;
   for (int i = 0; i < 8; ++i) {
-    std::cout << " " << ((i * 32));
+    std::cout << " " << ((i * 255) / 7);
     if (i != 7) {
       std::cout << ",";
     }
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_5_FLOOR[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::floor(i / 8.0);
+    std::cout << " " << std::floor((i * 31.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
@@ -53,7 +53,7 @@ int main(int argc, char **argv) {
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_4_FLOOR[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::floor(i / 16.0);
+    std::cout << " " << std::floor((i * 15.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_3_FLOOR[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::floor(i / 32.0);
+    std::cout << " " << std::floor((i * 7.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
@@ -78,7 +78,7 @@ int main(int argc, char **argv) {
 
 std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_5_CEIL[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::ceil(i / 8.0);
+    std::cout << " " << std::ceil((i * 31.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
@@ -90,7 +90,7 @@ std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_5_CEIL[256] = {" <<
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_4_CEIL[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::ceil(i / 16.0);
+    std::cout << " " << std::ceil((i * 15.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
@@ -102,7 +102,7 @@ std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_5_CEIL[256] = {" <<
 
   std::cout << "constexpr uint8_t Javelin::Data::BITSCALE_8_TO_3_CEIL[256] = {" << std::endl;
   for (int i = 0; i < 256; ++i) {
-    std::cout << " " << std::ceil(i / 32.0);
+    std::cout << " " << std::ceil((i * 7.0) / 255.0);
     if (i != 255) {
       std::cout << ",";
     }
